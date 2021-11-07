a2a625a542410c67ec7938059869717f50a4113e
Patrik Sandahl <patrik.sandahl@gmail.com>
2021-11-07 21:28:13 +0100

    Reference implementation for undistorsion of uv.

--- src/client/math/Helpers.ts
+++ src/client/math/Helpers.ts
@@ -30,3 +30,43 @@ export function uvToNdc(uv: Three.Vector2): Three.Vector2 {
 export function ndcToUv(ndc: Three.Vector2): Three.Vector2 {
     return ndc.clone().addScalar(1.0).multiplyScalar(0.5);
 }
+
+/**
+ * Undistort a UV coordinate.
+ * @param proj The projection matrix
+ * @param projInv The inverted projection matrix
+ * @param uv The distorted UV coordinate
+ * @param coeff The distortion coefficients k2, k3 and k4
+ * @returns The undistorted UV coordinate.
+ */
+export function undistortUv(
+    proj: Three.Matrix4,
+    projInv: Three.Matrix4,
+    uv: Three.Vector2,
+    coeff: Three.Vector3
+): Three.Vector2 {
+    const ndc2 = uvToNdc(uv);
+    const ndc = new Three.Vector3(ndc2.x, ndc2.y, 1.0);
+
+    const cam = ndc.applyMatrix4(projInv);
+    // Normalize by length.
+    cam.multiplyScalar(1.0 / cam.z);
+
+    const r = Math.hypot(cam.x, cam.y);
+    const r2 = r * r;
+    const r3 = r2 * r;
+    const r4 = r2 * r2;
+
+    const k2 = coeff.x;
+    const k3 = coeff.y;
+    const k4 = coeff.z;
+
+    const scale = 1.0 + (r2 * k2 + r3 * k3 + r4 * k4);
+    cam.multiplyScalar(scale);
+
+    // Set depth to 1.
+    cam.z = 1.0;
+
+    const adjNdc = cam.applyMatrix4(proj);
+    return ndcToUv(new Three.Vector2(adjNdc.x, adjNdc.y));
+}
