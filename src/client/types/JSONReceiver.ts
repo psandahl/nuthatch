021c922110271ab2e58e19523a0b0c59be381225
Patrik Sandahl <patrik.sandahl@gmail.com>
2021-12-20 07:58:14 +0100

    A little more comments.

--- src/client/types/JSONReceiver.ts
+++ src/client/types/JSONReceiver.ts
@@ -1,5 +1,19 @@
+/**
+ * Interface giving the contract of a class receiving JSON data.
+ */
 export interface JSONReceiver {
+    /**
+     * Receive a JSON object.
+     * @param obj The JSON object
+     * @param id The id used in the request
+     * @param url The url used in the request
+     */
     receiveJSONSucceeded(obj: object, id: number, url: string): void;
 
+    /**
+     * Notification that a JSON fetch has failed.
+     * @param id The id used in the request
+     * @param url The url used in the request
+     */
     receiveJSONFailed(id: number, url: string): void;
 }
