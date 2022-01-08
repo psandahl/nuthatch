ed7e7df4443eb067a968c0bca36fda9f068ee0aa
Patrik Sandahl <patrik.sandahl@gmail.com>
2022-01-08 21:48:50 +0100

    Basic flamingo.

--- src/client/entity/Simulator.ts
+++ src/client/entity/Simulator.ts
@@ -1,18 +1,23 @@
 import * as Three from 'three';
+import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
+
+import { fetchGLTF } from '../data/GLTFLoad';
+import { GLTFReceiver } from '../types/GLTFReceiver';
 
 class Simulation {
-    public constructor(
-        points: Three.Vector3[],
-        mesh: Three.Mesh,
-        velocity: number
-    ) {
+    public constructor(points: Three.Vector3[], model: GLTF, velocity: number) {
         this.group = new Three.Group();
         this.group.renderOrder = 1;
 
         this.curve = new Three.CatmullRomCurve3(points, true);
-        this.mesh = mesh;
+
+        this.mesh = model.scene.children[0].clone() as Three.Mesh;
         this.mesh.visible = false;
+        this.mesh.scale.set(0.2, 0.2, 0.2);
         this.mesh.position.copy(this.curve.getPoint(0));
+
+        this.mixer = new Three.AnimationMixer(this.mesh);
+        this.mixer.clipAction(model.animations[0]).setDuration(1).play();
 
         const trackGeometry = new Three.BufferGeometry().setFromPoints(
             this.curve.getPoints(100)
@@ -38,7 +43,15 @@ class Simulation {
         this.time += deltaMillis;
         if (this.running) {
             const lap = this.time % this.lapTime;
-            this.mesh.position.copy(this.curve.getPoint(lap / this.lapTime));
+
+            const t = lap / this.lapTime;
+            const position = this.curve.getPoint(t);
+            const direction = this.curve.getTangent(t);
+            this.mesh.up.copy(position.clone().normalize());
+            this.mesh.position.copy(this.curve.getPoint(t));
+            this.mesh.lookAt(position.clone().addScaledVector(direction, 1));
+
+            this.mixer.update(deltaMillis / 1000.0);
         } else {
             if (this.time > 1000) {
                 this.group.remove(this.track);
@@ -52,13 +65,17 @@ class Simulation {
     private group: Three.Group;
     private curve: Three.CatmullRomCurve3;
     private mesh: Three.Mesh;
+    private mixer: Three.AnimationMixer;
     private track: Three.Line;
     private lapTime: number;
     private time = 0;
     private running = false;
 }
 
-export class Simulator {
+/**
+ * A simple Catmull Rom simulator driving entities round a track.
+ */
+export class Simulator implements GLTFReceiver {
     public constructor() {
         this.scene = new Three.Scene();
 
@@ -68,6 +85,16 @@ export class Simulator {
         this.sphereGeometry = new Three.SphereGeometry(5);
         this.sphereMaterial = new Three.MeshBasicMaterial({ color: 0xff0000 });
         this.lineMaterial = new Three.LineBasicMaterial({ color: 0xff0000 });
+
+        fetchGLTF(1, 'models/Flamingo.glb', this);
+    }
+
+    public receiveGLTFSucceeded(model: GLTF, id: number, url: string): void {
+        this.simulatedModel = model;
+    }
+
+    public receiveGLTFFailed(id: number, url: string): void {
+        console.error(`Failed to receive GLTF model from ${url}`);
     }
 
     public createTrack(): void {
@@ -77,22 +104,17 @@ export class Simulator {
 
     public finalizeTrack(): void {
         if (this.trackPoints.length >= 2) {
-            // Todo: Make some more interesting mesh ...
-            const color = new Three.Color(
-                Math.random(),
-                Math.random(),
-                Math.random()
-            );
-
-            const sphere = new Three.Mesh(
-                this.sphereGeometry,
-                new Three.MeshBasicMaterial({ color: color.getHex() })
-            );
-
-            const simulation = new Simulation(this.trackPoints, sphere, 20);
-            this.simulations.push(simulation);
-
-            this.scene.add(simulation.getGroup());
+            if (this.simulatedModel) {
+                const simulation = new Simulation(
+                    this.trackPoints,
+                    this.simulatedModel,
+                    20
+                );
+                this.simulations.push(simulation);
+                this.scene.add(simulation.getGroup());
+            } else {
+                console.error('Failed to add model to simulation');
+            }
         }
 
         this.scene.remove(this.markerGroup);
@@ -111,7 +133,7 @@ export class Simulator {
             // Assume geocentric coordinate, where direction is same as point.
             const direction = point.clone().normalize();
 
-            // Ad hoc. Add 50 meters above the selected point.
+            // Ad hoc. Add 20 meters above the selected point.
             const trackPoint = point.clone().addScaledVector(direction, 20);
             this.trackPoints.push(trackPoint);
 
@@ -148,9 +170,10 @@ export class Simulator {
     private markerGroup: Three.Group;
     private trackPoints: Three.Vector3[] = [];
 
-    private simulations: Simulation[] = [];
-
     private sphereGeometry: Three.SphereGeometry;
     private sphereMaterial: Three.MeshBasicMaterial;
     private lineMaterial: Three.LineBasicMaterial;
+
+    private simulatedModel?: GLTF;
+    private simulations: Simulation[] = [];
 }
